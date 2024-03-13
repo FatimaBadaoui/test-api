c90d3ce1767857e1b14d8c0d23e369b05a202224
Fatima Badaoui <fatimina615@gmail.com>
2024-03-13 15:25:21 +0100

    add weather api

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import express from "express";
 
 // console.log(process.argv); // array of node args "node server.js development" => ["node_path", "file_path of server.js", "development"]
-
+const API_KEY = process.env.API_KEY;
 const PORT = process.env.PORT || 8000;
 const app = express();
 // const environment = "development"; // or "production" in this way we have to manually change it every time
 // const environment = process.argv[2] // getting it from node arguments;
 const environment = process.env.NODE_ENV; // to use the variable inside the .env we need to write node --env-file=.env server.js (node has to be updated)
 // process.env is an object
+
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Test API! endpoints: /user-info and /random-color");
@@ -31,6 +33,24 @@ app.get("/random-color", (req, res) => {
   }
 
   res.json({ hexColor: color });
+});
+
+app.get("/weather/:location", async (req, res) => {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${req.params.location}&appid=${API_KEY}&units=metric`
+  );
+  const data = await response.json();
+
+  res.json({
+    location: data.name,
+    country: data.sys.country,
+    temp: data.main.temp,
+    minTemp: data.main.temp_min,
+    maxTemp: data.main.temp_max,
+    humidity: data.main.humidity,
+    description: data.weather[0].description,
+  });
+  // res.json(data);
 });
 
 app.listen(PORT, () => {
